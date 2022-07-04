6e1a07a8e67b1c74b103e1aab3816dfeef5dcff9
italoDutr4 <italoshanti@gmail.com>
2022-07-03 22:22:22 -0300

    projeto base

--- src/main/java/percentageStates.java
+++ src/main/java/percentageStates.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class percentageStates {
+public class percentageStates {
+        public static void main(String[] args){
+                double sp = 67.83643;
+                double rj = 36.67866;
+                double mg = 29.22988;
+                double es = 27.16548;
+                double out = 19.84953;
+                double total = sp + rj + mg + es + out;
+
+                System.out.println(total);
+
+                double percStateSp = sp/total*100;
+                double percStateRj = rj/total*100;
+                double percStateMg = mg/total*100;
+                double percStateEs = es/total*100;
+                double percStateOut = out/total*100;
+
+                System.out.println("A porcentagem de SP é" + " "+ percStateSp + "%");
+                System.out.println("A porcentagem de RJ é" + " "+ percStateRj + "%");
+                System.out.println("A porcentagem de MG é" + " "+ percStateMg + "%");
+                System.out.println("A porcentagem de ES é" + " "+ percStateEs + "%");
+                System.out.println("A porcentagem de Outros é"+ " "+ percStateOut + "%");
+        }
 }
